c076e87e31aebc91ef4b0e1f54592d2dcef0aa7b
roy <1084577447@qq.com>
2022-04-13 22:53:33 +0800

    旋转数组

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -5,8 +5,10 @@ public class Solution {
     public static void main(String[] args) {
 //        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
 //        System.out.println(Arrays.toString(removeDuplicates(nums)));
-        int[] prices = {5, 6, 4, 3, 3, 2, 6, 7, 8};
-        System.out.println(maxProfit2(prices));
+//        int[] prices = {5, 6, 4, 3, 3, 2, 6, 7, 8};
+//        System.out.println(maxProfit2(prices));
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        rotate(nums, 3);
     }
 
     // 删除排序数组中的重复项
@@ -64,5 +66,19 @@ public class Solution {
             }
         }
         return profile;
+    }
+
+    // 旋转数组
+    public static void rotate(int[] nums, int k) {
+        int realPosition = k % nums.length;// 实际向右轮转的位置
+        int[] newNums = Arrays.copyOf(nums, nums.length + realPosition);
+        for (int i = newNums.length - 1; i >= 0; i--) {
+            if (i > realPosition - 1) {
+                newNums[i] = newNums[i - realPosition];
+            } else {
+                newNums[i] = newNums[i + nums.length];
+            }
+        }
+        System.arraycopy(newNums, 0, nums, 0, nums.length);
     }
 }
